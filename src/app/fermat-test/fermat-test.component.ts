496984426d387d61f156bca7d76028f3401db633
henriettxhp <henriette.rupp@t-online.de>
2019-08-27 18:22:27 +0200

    2 files modified

--- src/app/fermat-test/fermat-test.component.ts
+++ src/app/fermat-test/fermat-test.component.ts
@@ -6,11 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fermat-test.component.css']
 })
 export class FermatTestComponent implements OnInit {
+
   versuchskaninchen: number = 13
   anzahl: number = 5
   ergebnis= "noch nicht berechnet..."
 
   constructor() { }
+  fermattest() {
+    this.ergebnis = 'Starte Fermat-Test der Zahl ' + this.versuchskaninchen + '...'
+    let zeuge = 2;
+    // Berechne zeuge^(versuchskaninchen-1) modulo versuchskaninchen
+    let zwischenergebnis = 1;
+    for(let i = 0; i < this.versuchskaninchen-1; i++) {
+      zwischenergebnis = zwischenergebnis * zeuge;
+    }
+    zwischenergebnis = zwischenergebnis % this.versuchskaninchen;
+    if(zwischenergebnis === 1) {
+      this.ergebnis += ' Zeuge ' + zeuge + ' OK... ';
+    } else{
+      this.ergebnis += ' Zeuge ' + zeuge + ' NICHT OK! '
+    }
+
+  }
 
   ngOnInit() {
   }
