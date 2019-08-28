1baa1be1fcc756c52448718e08f27430ab550db5
henriettxhp <henriette.rupp@t-online.de>
2019-08-28 10:15:25 +0200

    2 files modified

--- src/app/fermat-test/fermat-test.component.ts
+++ src/app/fermat-test/fermat-test.component.ts
@@ -9,24 +9,36 @@ export class FermatTestComponent implements OnInit {
 
   versuchskaninchen: number = 13
   anzahl: number = 5
-  ergebnis= "noch nicht berechnet..."
+  ergebnis = "noch nicht berechnet..."
 
   constructor() { }
   fermattest() {
     this.ergebnis = 'Starte Fermat-Test der Zahl ' + this.versuchskaninchen + '...'
-    let zeuge = 2;
-    // Berechne zeuge^(versuchskaninchen-1) modulo versuchskaninchen
-    let zwischenergebnis = 1;
-    for(let i = 0; i < this.versuchskaninchen-1; i++) {
-      zwischenergebnis = zwischenergebnis * zeuge;
-    }
-    zwischenergebnis = zwischenergebnis % this.versuchskaninchen;
-    if(zwischenergebnis === 1) {
-      this.ergebnis += ' Zeuge ' + zeuge + ' OK... ';
-    } else{
-      this.ergebnis += ' Zeuge ' + zeuge + ' NICHT OK! '
-    }
+    let endergebnis = true;
+    for (let zeuge = 2; zeuge < this.anzahl + 2; zeuge++) {
 
+      // Berechne zeuge^(versuchskaninchen-1) modulo versuchskaninchen
+      let zwischenergebnis = 1;
+      for (let i = 0; i < this.versuchskaninchen - 1; i++) {
+        zwischenergebnis = zwischenergebnis * zeuge;
+        zwischenergebnis = zwischenergebnis % this.versuchskaninchen;
+      }
+      
+      if (zwischenergebnis === 1) {
+        this.ergebnis += ' Zeuge ' + zeuge + ' OK... ';
+      } else {
+        this.ergebnis += ' Zeuge ' + zeuge + ' NICHT OK! '
+        endergebnis = false
+        break;
+      }
+
+    }
+    if (endergebnis === true) {
+      this.ergebnis += 'Test bestanden'
+    }
+    else {
+      this.ergebnis += 'Test NICHT bestanden'
+    }
   }
 
   ngOnInit() {
